af655e9e361769d6ce62d81037392027df0bb30e
Daniel Butler <cactusfly@gmail.com>
2016-10-31 19:17:58 -0400

    added a fire feature and test suite

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -9,7 +9,7 @@ function checkForShip (player, coordinates) {
     })[0];
 
     if (shipPresent) {
-      return true;
+      return ship;
     }
   }
 
@@ -20,5 +20,14 @@ function damageShip (ship, coordinates) {
   ship.damage.push(coordinates);
 }
 
+function fire (player, coordinates) {
+  var ship = checkForShip(player, coordinates);
+
+  if (ship) {
+    damageShip(ship, coordinates);
+  }
+}
+
 module.exports.checkForShip = checkForShip;
 module.exports.damageShip = damageShip;
+module.exports.fire = fire;
